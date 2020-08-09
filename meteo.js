d4814927e667046e0ebb40da2983b93ae558793a
tchiffaMed <mahamadou.tchiffa@codeloccol.org>
2020-08-09 23:54:05 +0100

    try

---
+++ meteo.js
@@ -0,0 +1,44 @@
+                  
+/**
+ * Moves the map to display over Berlin
+ *
+ * @param  {H.Map} map      A HERE Map instance within the application
+ */
+function moveMapToBerlin(map){
+    map.setCenter({lat:17.5968801 , lng:8.0828506});
+    map.setZoom(14);
+  }
+  
+  /**
+   * Boilerplate map initialization code starts below:
+   */
+  
+  //Step 1: initialize communication with the platform
+  // In your own code, replace variable window.apikey with your own apikey
+  var platform = new H.service.Platform({
+    apikey: 'S4NJ4haf_Vc2IEOTxcSlVqA3Vo3HUFIkulP6MKXS9Nc'
+  });
+  var defaultLayers = platform.createDefaultLayers();
+  
+  //Step 2: initialize a map - this map is centered over Europe
+  var map = new H.Map(document.getElementById('map'),
+    defaultLayers.vector.normal.map,{
+    center: {lat:17, lng:8},
+    zoom: 4,
+    pixelRatio: window.devicePixelRatio || 1
+  });
+  // add a resize listener to make sure that the map occupies the whole container
+  window.addEventListener('resize', () => map.getViewPort().resize());
+  
+  //Step 3: make the map interactive
+  // MapEvents enables the event system
+  // Behavior implements default interactions for pan/zoom (also on mobile touch environments)
+  var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+  
+  // Create the default UI components
+  var ui = H.ui.UI.createDefault(map, defaultLayers);
+  
+  // Now use the map as required...
+  window.onload = function () {
+    moveMapToBerlin(map);
+  }
